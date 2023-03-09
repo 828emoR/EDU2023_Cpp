864075451fc0487f8d6c6975778c7a1f08df7b10
emoR 828 <rlawnghks072@naver.com>
2023-03-09 14:36:14 +0900

    230309 수업.

--- FirstTime.cpp
+++ FirstTime.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 using namespace std;
-
-int main()
+// #define = 매크로를 정의하는 지시문.(#define identfire token-list(상수 정의), #define identifire(parameter-list) token-list(함수 정의))
+//int main()
+//{
+//
+//	cout << "Hello World" << endl;
+//	return 0;
+//
+//
+//}
+void main()
 {
+	cout << "이름을 입력해주세요 " << endl;
 
+	char name[256];
+	cin >> name;
 
-	cout << "Hello World" << endl;
-	return 0;
+	cout << name << "님 C++세계에 오신것을 환영합니다. " << endl;
+
 }
+
+//cout 과 cin 은 꼭 개념을 알아두자.
